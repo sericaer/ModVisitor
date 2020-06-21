d82ee16e15c229c7ab232a146a1c87679cac9e35
sericaer <sericaer@outlook.com>
2020-06-21 18:42:58 +0800

    init

---
+++ ModVisitor.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ModVisitor
+{
+    [System.AttributeUsage(System.AttributeTargets.Field)]
+    public class ModVisit : System.Attribute
+    {
+    }
+
+    internal class Setter : Visitor
+    {
+        public Setter(string script)
+        {
+            Parse(script, VisitType.WRITE);
+        }
+
+        internal void set(object value)
+        {
+            var currData = dictData[innerName];
+
+            for (int i = 0; i < fieldDepths.Count() - 1; i++)
+            {
+                var currField = fieldDepths[i];
+                currData = currField.GetValue(currData);
+            }
+
+            fieldDepths.Last().SetValue(currData, value);
+        }
+    }
+
+    internal class Getter : Visitor
+    {
+        public Getter(string script)
+        {
+            Parse(script, VisitType.READ);
+        }
+
+        internal object get()
+        {
+            if(is_static)
+            {
+                return raw;
+            }
+
+            var currData = dictData[innerName];
+
+            for (int i = 0; i < fieldDepths.Count() - 1; i++)
+            {
+                var currField = fieldDepths[i];
+                currData = currField.GetValue(currData);
+            }
+
+            return fieldDepths.Last().GetValue(currData);
+        }
+    }
+
+    internal class Visitor
+    {
+        internal enum VisitType
+        {
+            READ,
+            WRITE
+        }
+
+        protected static Dictionary<string, object> dictData = new Dictionary<string, object>();
+
+        protected static Dictionary<string, List<ReflectionInfo>> dictReflect = new Dictionary<string, List<ReflectionInfo>>();
+
+        internal static void InitData(string key, object gmdata)
+        {
+            dictData.Add(key, gmdata);
+        }
+
+        internal static void InitReflect(string key, Type type)
+        {
+            dictReflect.Add(key, ParseReflectionInfo(type));
+        }
+
+        static List<ReflectionInfo> ParseReflectionInfo(Type type)
+        {
+            var rslt = new List<ReflectionInfo>();
+            var fields = type.GetFields(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
+            foreach (var field in fields.Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(ModVisit))))
+            {
+                rslt.Add(new ReflectionInfo(field, ParseReflectionInfo(field.FieldType)));
+
+            }
+            return rslt;
+        }
+
+        internal void Parse(string script, VisitType vistType)
+        {
+            raw = script;
+
+            int start = 0;
+
+            List<ReflectionInfo> rootReflection = null;
+            ReflectionInfo currReflection = null;
+
+            while (start < raw.Length)
+            {
+                var matched = Regex.Match(raw.Substring(start), @"^[A-Za-z]+\.*");
+                if (!matched.Success)
+                {
+                    throw new Exception();
+                }
+
+                var matchedValue = matched.Value.TrimEnd('.');
+
+                try
+                {
+                    if (start == 0)
+                    {
+                        if (!dictReflect.ContainsKey(matchedValue))
+                        {
+                            if(vistType == VisitType.WRITE)
+                            {
+                                throw new Exception();
+                            }
+
+                            if (matched.Length != raw.Length)
+                            {
+                                throw new Exception();
+                            }
+
+                            return;
+                        }
+
+                        rootReflection = dictReflect[matchedValue];
+                        innerName = matchedValue;
+                    }
+                    else
+                    {
+
+                        if (currReflection == null)
+                        {
+                            currReflection = rootReflection.Single(x => x.field.Name == matchedValue);
+                        }
+                        else
+                        {
+                            currReflection = currReflection.subs.Single(x => x.field.Name == matchedValue);
+                        }
+
+                        fieldDepths.Add(currReflection.field);
+                    }
+
+                    start += matched.Length;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Parse failed! '{matchedValue}' in '{script}' ", e);
+                }
+
+                
+            }
+        }
+
+        internal bool is_static
+        {
+            get
+            {
+                return fieldDepths.Count() == 0;
+            }
+        }
+
+        internal string raw;
+
+        protected string innerName;
+        protected List<FieldInfo> fieldDepths = new List<FieldInfo>();
+
+        protected class ReflectionInfo
+        {
+            internal FieldInfo field;
+            internal List<ReflectionInfo> subs = new List<ReflectionInfo>();
+
+            public ReflectionInfo(FieldInfo field, List<ReflectionInfo> subs)
+            {
+                this.field = field;
+                this.subs = subs;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using ModVisitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReflectionVisitor
+{
+    public class GMData
+    {
+        [ModVisit]
+        public Taishou taishou;
+
+        [ModVisit]
+        public int year;
+
+        public int score;
+
+        public GMData()
+        {
+            taishou = new Taishou();
+        }
+    }
+
+    public class Taishou
+    {
+        [ModVisit]
+        public string name;
+
+        [ModVisit]
+        public int age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Visitor.InitReflect("GMData", typeof(GMData));
+            
+            var script = "GMData.taishou.name";
+
+            Setter set = new Setter(script);
+
+            var gmdata = new GMData();
+            Visitor.InitData("GMData", gmdata);
+
+            set.set("test_name");
+
+            Console.WriteLine(gmdata.taishou.name);
+
+            Getter get = new Getter(script);
+
+            Console.WriteLine(get.get());
+
+        }
+    }
+
+    
+}
