fc80d3b81848352a0aa7aafceac84c0bc0c2bd9c
sericaer <sericaer@outlook.com>
2020-06-23 11:12:15 +0800

    add

--- ModVisitor.cs
+++ ModVisitor.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ModVisitor
 {
-    [System.AttributeUsage(System.AttributeTargets.Field)]
+    [System.AttributeUsage(System.AttributeTargets.Field | AttributeTargets.Property)]
     public class ModVisit : System.Attribute
     {
     }
@@ -24,13 +24,13 @@ namespace ModVisitor
         {
             var currData = dictData[innerName];
 
-            for (int i = 0; i < fieldDepths.Count() - 1; i++)
+            for (int i = 0; i < refElements.Count() - 1; i++)
             {
-                var currField = fieldDepths[i];
-                currData = currField.GetValue(currData);
+                var currField = refElements[i];
+                currData = currField.Get(currData);
             }
 
-            fieldDepths.Last().SetValue(currData, value);
+            refElements.Last().Set(currData, value);
         }
     }
 
@@ -38,34 +38,38 @@ namespace ModVisitor
     {
         public Getter(string script)
         {
+            FuncSetStaticValue = (obj)=> staticValue = obj;
+
             Parse(script, VisitType.READ);
         }
 
         public object get()
         {
-            if(is_static)
+            if(staticValue != null)
             {
-                return raw;
+                return staticValue;
             }
 
             var currData = dictData[innerName];
 
-            for (int i = 0; i < fieldDepths.Count() - 1; i++)
+            for (int i = 0; i < refElements.Count() - 1; i++)
             {
-                var currField = fieldDepths[i];
-                currData = currField.GetValue(currData);
+                var currField = refElements[i];
+                currData = currField.Get(currData);
             }
 
-            return fieldDepths.Last().GetValue(currData);
+            return refElements.Last().Get(currData);
         }
+
+        public object staticValue;
     }
 
     public class Visitor
     {
         internal enum VisitType
         {
-            READ,
-            WRITE
+            READ = 0x01,
+            WRITE = 0x10
         }
 
         protected static Dictionary<string, object> dictData = new Dictionary<string, object>();
@@ -90,19 +94,42 @@ namespace ModVisitor
         static List<ReflectionInfo> ParseReflectionInfo(Type type)
         {
             var rslt = new List<ReflectionInfo>();
+
+            var properies = type.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
             var fields = type.GetFields(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
+
             foreach (var field in fields.Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(ModVisit))))
             {
-                rslt.Add(new ReflectionInfo(field, ParseReflectionInfo(field.FieldType)));
-
+                rslt.Add(new ReflectionInfo(new ReflectionInfo.Field(field), ParseReflectionInfo(field.FieldType)));
             }
+            foreach (var property in properies.Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(ModVisit))))
+            {
+                rslt.Add(new ReflectionInfo(new ReflectionInfo.Property(property), ParseReflectionInfo(property.PropertyType)));
+            }
+
             return rslt;
         }
 
         internal void Parse(string script, VisitType vistType)
         {
+
             raw = script;
 
+            if (TryParseDigitCalc(script, vistType))
+            {
+                return;
+            }
+
+            if (TryParseModExpr(script, vistType))
+            {
+                return;
+            }
+
+            throw new Exception($"parse string faild! script:{script}, visit type:{vistType}");
+        }
+
+        private bool TryParseModExpr(string script, VisitType vistType)
+        {
             int start = 0;
 
             List<ReflectionInfo> rootReflection = null;
@@ -113,80 +140,199 @@ namespace ModVisitor
                 var matched = Regex.Match(raw.Substring(start), @"^[A-Za-z_]+\.*");
                 if (!matched.Success)
                 {
-                    throw new Exception();
+                    return false;
                 }
 
                 var matchedValue = matched.Value.TrimEnd('.');
 
-                try
+                if (start == 0)
                 {
-                    if (start == 0)
+                    if (!dictReflect.ContainsKey(matchedValue))
                     {
-                        if (!dictReflect.ContainsKey(matchedValue))
+                        if (vistType == VisitType.WRITE)
                         {
-                            if(vistType == VisitType.WRITE)
-                            {
-                                throw new Exception();
-                            }
-
-                            if (matched.Length != raw.Length)
-                            {
-                                throw new Exception();
-                            }
-
-                            return;
+                            return false;
                         }
 
-                        rootReflection = dictReflect[matchedValue];
-                        innerName = matchedValue;
+                        if (matched.Length != raw.Length)
+                        {
+                            return false;
+                        }
+
+                        FuncSetStaticValue(matchedValue);
+                        return true;
+                    }
+
+                    rootReflection = dictReflect[matchedValue];
+                    innerName = matchedValue;
+                }
+                else
+                {
+
+                    if (currReflection == null)
+                    {
+                        currReflection = rootReflection.Single(x => x.element.Name == matchedValue);
                     }
                     else
                     {
-
-                        if (currReflection == null)
-                        {
-                            currReflection = rootReflection.Single(x => x.field.Name == matchedValue);
-                        }
-                        else
-                        {
-                            currReflection = currReflection.subs.Single(x => x.field.Name == matchedValue);
-                        }
-
-                        fieldDepths.Add(currReflection.field);
+                        currReflection = currReflection.subs.Single(x => x.element.Name == matchedValue);
                     }
 
-                    start += matched.Length;
-                }
-                catch (Exception e)
-                {
-                    throw new Exception($"Parse failed! '{matchedValue}' in '{script}' ", e);
+                    if ((vistType == VisitType.READ && !currReflection.element.canRead)
+                        || (vistType == VisitType.WRITE && !currReflection.element.canWrite))
+                    {
+                        return false;
+                    }
+
+                    refElements.Add(currReflection.element);
                 }
 
-                
+                start += matched.Length;
             }
+
+            return true;
         }
 
-        internal bool is_static
+        private bool TryParseDigitCalc(string script, VisitType vistType)
         {
-            get
+            if(vistType == VisitType.WRITE)
             {
-                return fieldDepths.Count() == 0;
+                return false;
             }
+
+            var convert = script.Replace(" ", "");
+
+            var rslt = Regex.Match(convert, @"^[\+\-]?[0-9]+\.?[0-9]+(*[\+\-\*/]?[0-9]+\.?[0-9]+)*");
+            if (!rslt.Success)
+            {
+                return false;
+            }
+            if (rslt.Length != convert.Length)
+            {
+                return false;
+            }
+
+            double value = 0.0;
+
+            int start = 0;
+            while (start < raw.Length)
+            {
+                if (start == 0)
+                {
+                    var matched_head = Regex.Match(raw.Substring(start), @"^[\+\-]?[0-9]+\.?[0-9]+");
+                    if (!matched_head.Success)
+                    {
+                        return false;
+                    }
+
+                    value = double.Parse(matched_head.Value);
+                    start += matched_head.Length;
+                    continue;
+                }
+
+                var matched = Regex.Match(raw.Substring(start), @"^[\+\-\*/][0-9]+\.?[0-9]+");
+                if (!matched.Success)
+                {
+                    return false;
+                }
+
+                if (matched.Value.StartsWith("+"))
+                {
+                    value += double.Parse(matched.Value.Replace("+", ""));
+                }
+                if (matched.Value.StartsWith("-"))
+                {
+                    value -= double.Parse(matched.Value.Replace("-", ""));
+                }
+                if (matched.Value.StartsWith("*"))
+                {
+                    value *= double.Parse(matched.Value.Replace("*", ""));
+                }
+                if (matched.Value.StartsWith("/"))
+                {
+                    value /= double.Parse(matched.Value.Replace("/", ""));
+                }
+            }
+
+            FuncSetStaticValue(value);
+            return true;
         }
 
         internal string raw;
 
         protected string innerName;
-        protected List<FieldInfo> fieldDepths = new List<FieldInfo>();
+        protected List<ReflectionInfo.Element> refElements = new List<ReflectionInfo.Element>();
+        protected Action<object> FuncSetStaticValue;
 
         protected class ReflectionInfo
         {
-            internal FieldInfo field;
+            public abstract class Element
+            {
+                public abstract string Name{ get;}
+                public abstract bool canRead { get; }
+                public abstract bool canWrite { get; }
+                public abstract object Get(object obj);
+                public abstract void Set(object obj, object value);
+            }
+
+            public class Field : Element
+            {
+                internal FieldInfo field;
+
+                public Field(FieldInfo field)
+                {
+                    this.field = field;
+                }
+
+                public override string Name => field.Name;
+
+                public override bool canRead => true;
+
+                public override bool canWrite => true;
+
+                public override object Get(object obj)
+                {
+                    return field.GetValue(obj);
+                }
+
+                public override void Set(object obj, object value)
+                {
+                    field.SetValue(obj, value);
+                }
+            }
+
+            public class Property : Element
+            {
+                internal PropertyInfo property;
+
+                public Property(PropertyInfo property)
+                {
+                    this.property = property;
+                }
+
+                public override string Name => property.Name;
+
+                public override bool canRead => property.CanRead;
+
+                public override bool canWrite => property.CanWrite;
+
+                public override object Get(object obj)
+                {
+                    return property.GetValue(obj);
+                }
+
+                public override void Set(object obj, object value)
+                {
+                    property.SetValue(obj, value);
+                }
+            }
+
+            internal Element element;
             internal List<ReflectionInfo> subs = new List<ReflectionInfo>();
 
-            public ReflectionInfo(FieldInfo field, List<ReflectionInfo> subs)
+            internal ReflectionInfo(Element element, List<ReflectionInfo> subs)
             {
-                this.field = field;
+                this.element = element;
                 this.subs = subs;
             }
         }
